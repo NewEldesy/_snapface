bbd202c22e404eb260ae3d287e39ee4fcc606129
*Eldesy <*New_Eldesy@hotmail.com*>
2024-06-07 16:12:14 +0000

    07/06/2024

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Facesnap } from '../models/face-snap';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit{
+  @Input() faceSnap!: Facesnap;
+
+  hasSnapped!: boolean;
+  SnapButtonText!: String;
+
+  ngOnInit(): void {
+    this.SnapButtonText = 'Oh Snap!';
+    this.hasSnapped = false;
+  }
+
+  onSnap(): void {
+    if (this.hasSnapped) {
+      this.unSnap();
+    } else {
+      this.addSnap();
+    }
+  }
+
+  unSnap(){
+    this.faceSnap.removeSnap();
+    this.SnapButtonText = 'oh Snap!';
+    this.hasSnapped = false;
+  }
+
+  addSnap(){
+    this.faceSnap.addSnap();
+    this.SnapButtonText = 'Oops, Unsnap!';
+    this.hasSnapped = true;
+  }
+}
---
+++ src/app/models/face-snap.ts
@@ -0,0 +1,43 @@
+// Method 1
+// export class Facesnap {
+//     title!: String;
+//     description!: String;
+//     createdAt!: Date;
+//     snaps!: number;
+//     imageUrl!: String;
+
+//     constructor(title: string, description: string, createdAt: Date, snaps: number, imageUrl: string){
+//         this.title;
+//         this.description;
+//         this.createdAt;
+//         this.snaps;
+//         this.imageUrl;
+//     }
+// }
+
+
+// Method 2
+export class Facesnap {
+
+    //Pour dire que ce n'est pas une variable obligatoire
+    location?: string;
+
+    constructor(public title: string,
+        public description: string,
+        public createdAt: Date,
+        public snaps: number,
+        public imageUrl: string){}
+    
+        addSnap(): void {
+            this.snaps++;
+        }
+
+        removeSnap(): void {
+            this.snaps--;
+        }
+
+        setLocation(location: string): void{
+            this.location = location;
+        }
+}
+
